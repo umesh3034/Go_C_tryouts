314762c5e6561f1043c785693040d3e3797ae8da
umesh3034 <umesh.freelance@gmail.com>
2020-02-19 07:30:52 +0000

    updates struct examples

--- Go/struct_examples.go
+++ Go/struct_examples.go
@@ -40,6 +40,79 @@ func main() {
 
 	emp5 := Employee{
 		firstName: "umesh",
-		lastName: ,
+		lastName:  "keerthy",
 	}
+
+	fmt.Println("emp5", emp5)
+
+	emp6 := Employee{"sam", "anderson", 55, 6000}
+	fmt.Println("first", emp6.firstName)
+	fmt.Println("last", emp6.lastName)
+
+	var emp7 Employee
+	emp7.firstName = "thomas"
+	fmt.Println("emp7", emp7)
+
+	emp8 := &Employee{"jimmy", "choo", 55, 6000}
+	fmt.Println("first", (*emp8).firstName)
+	fmt.Println("last", (*emp8).lastName)
+
+	fmt.Println("first", emp8.firstName)
+	fmt.Println("last", emp8.lastName)
+
+	type Person struct {
+		string
+		int
+	}
+
+	p := Person{"naveen", 50}
+	fmt.Println(p)
+
+	p.string = "Jose"
+	p.int = 100
+
+	fmt.Println(p)
+
+	type Address struct {
+		city, state string
+	}
+
+	type Person2 struct {
+		name    string
+		age     int
+		address Address
+	}
+
+	var p1 Person2
+
+	p1.name = "umesh"
+	p1.age = 35
+	p1.address = Address{
+		city:  "bangalore",
+		state: "KA",
+	}
+
+	fmt.Println("name", p1.name)
+	fmt.Println("age", p1.age)
+	fmt.Println("city", p1.address.city)
+	fmt.Println("state", p1.address.state)
+
+	//promoted fields
+	type Person3 struct {
+		name string
+		age  int
+		Address
+	}
+
+	var p3 Person3
+	p3.name = "Naveen"
+	p3.age = 50
+	p3.Address = Address{
+		city:  "Chicago",
+		state: "Illinois",
+	}
+	fmt.Println("Name:", p3.name)
+	fmt.Println("Age:", p3.age)
+	fmt.Println("City:", p3.city)   //city is promoted field
+	fmt.Println("State:", p3.state) //state is promoted field
 }
